d8df870c6bc0c983abd0c263572f0bb63db6d3c0
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-21 19:54:51 -0400

    Add route for add-trip

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/add-trip', function(req, res, next) {
+  res.render('add-trip');
+});
 
 module.exports = router;
