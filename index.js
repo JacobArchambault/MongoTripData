e06f12de393769b48cfff69b5221c7514a38047f
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-20 22:05:07 -0400

    Send static file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-let app = express();                  // create the express app
+const app = express();                  // create the express app
 
 app.use('/', (_req, res) =>      // when the default route is requested...
-    res.send('Hello World!')         // call this function and send a response
+    res.sendFile('index.html', {root: '.'})         // call this function and send a response
 );
 
 app.listen(3000, function () {
