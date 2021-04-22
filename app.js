ca6667c8efae1642083c4701b6eccf6680de5899
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-21 23:03:31 -0400

    Move add-trip into its own route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const addTripRouter = require('./routes/add-trip')
 const app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/add-trip', addTripRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
