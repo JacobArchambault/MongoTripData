8ff4e3b4baee7a97151de2d658c1aa486bca698c
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-23 18:26:13 -0400

    Get display (mostly) working for index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var Trip = require('../models/trips');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    Trip.find(function(err, trip) {
+      res.render('index', { trips: trip });
+  });
 });
-
 module.exports = router;
