59b0243522bcb5dc6f5fef2493bfc172c8bc85e7
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-25 22:53:00 -0400

    Get post working

--- routes/add-trip.js
+++ routes/add-trip.js
@@ -1,13 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+var Trip = require('../models/trips');
 
 
 router.get('/', function (req, res, next) {
   res.render('add-trip');
 });
 
-router.post('/', function(req, res){
-  return res.redirect('/');
+router.post('/', function (req, res) {
+  var newItem = new Trip({
+    date: req.body.date,
+    city: req.body.city,
+    miles: req.body.miles,
+    gallons: req.body.gallons
+  });
+
+  newItem.save(function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else {
+      res.send("Car successfully added.");
+    }
+  });
 });
 
 module.exports = router;
