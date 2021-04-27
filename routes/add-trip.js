0aed9e0069a7e1f9057bb2fd97a7ffe90cdfda91
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-26 23:29:26 -0400

    Use lambdas for add-trip.js

--- routes/add-trip.js
+++ routes/add-trip.js
@@ -3,17 +3,14 @@ const router = express.Router();
 const Trip = require('../models/trips');
 
 
-router.get('/', function (req, res, next) {
-  res.render('add-trip');
-});
+router.get('/', (req, res, next) => res.render('add-trip'));
 
-router.post('/', function (req, res) {
+router.post('/', (req, res) =>
   new Trip({
     date: req.body.date,
     city: req.body.city,
     miles: req.body.miles,
     gallons: req.body.gallons
-  }).save(() => res.redirect("/"));
-});
+  }).save(() => res.redirect("/")));
 
 module.exports = router;
