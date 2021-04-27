41a47a615e6471f1499d798bd0f54a79c509271e
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-26 22:33:20 -0400

    Inline trip array

--- routes/initialize.js
+++ routes/initialize.js
@@ -6,13 +6,12 @@ var Trips = require('../models/trips.js');
 
 router.get('/', function (req, res) {
     /* data should be in JSON format */
-    var allTrips = [
+    Trips.insertMany([
         { "date": "2021-11-01", "city": "Chicago", "miles": 300, "gallons": 40 },
         { "date": "2021-10-04", "city": "Nashville", "miles": 350, "gallons": 100 },
         { "date": "2020-11-02", "city": "Cincinnati", "miles": 400, "gallons": 125 },
         { "date": "2020-09-03", "city": "Indianapolis", "miles": 95, "gallons": 10 },
-    ]
-    Trips.insertMany(allTrips, function (err, docs) {
+    ], function (err, docs) {
         if (err) {
             res.send('Error: ' + err);
         }
