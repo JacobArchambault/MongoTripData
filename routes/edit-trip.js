604b8466b127535565a1ddfa02a6f5fa2491cb2f
Jacob Archambault <jacobarchambault@gmail.com>
2021-04-28 23:12:16 -0400

    Get findOneAndUpdate working with constants

--- routes/edit-trip.js
+++ routes/edit-trip.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Trip = require('../models/trips');
 const hbs = require('hbs');
-hbs.registerHelper('select', function(selected, option) {
+hbs.registerHelper('select', function (selected, option) {
   return (selected == option) ? 'selected="selected"' : '';
 });
 
 
-router.get('/', (req, res, next) => Trip.findOne({_id: req.query._id}, (err, trip) => res.render('edit-trip', { trips: trip })));
+router.get('/', (req, res, next) => Trip.findOne({ _id: req.query._id }, (err, trip) => res.render('edit-trip', { trips: trip })));
 
-router.post('/', (req, res) =>
-  new Trip({
-    date: req.body.date,
-    city: req.body.city,
-    miles: req.body.miles,
-    gallons: req.body.gallons
-  }).save(() => res.redirect("/")));
+router.post('/', function (req, res) {
+  Trip.findOneAndUpdate({
+    'city': 'Chicago'
+  },
+    {
+      $set: {
+        miles: 1000,
+        gallons: 2
+      }
+    }, { upsert: false }, function (err, doc) { res.redirect("/"); })
+});
 
 module.exports = router;
